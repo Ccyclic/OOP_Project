149467bf7e1366bc6a888b6cf2e7654071dd73f5
Ccyclic <149557025+Ccyclic@users.noreply.github.com>
2024-12-12 13:54:48 +0700

    zz

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,7 @@
 import display.Window;
 import game.scenes.StartMenu;
 
-public class aMain {
+public class main {
     public static void main(String[] args) {
         Window win = new Window();
         win.init("Dungeon Crawler", 1280, 720);
